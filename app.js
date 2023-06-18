018c54c5217f6dbfb6991603a6f5706414495564
Ali Khan <merabhai786786@gmail.com>
2023-06-18 14:59:53 +0500

    adding empty message to show when there is no test

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let btn= document.querySelector('.add-btn');
-        
+let ul_list= document.querySelector("#ul_list");
+let lists =document.querySelector(".lists")      
 let input= document.querySelector('.input');
 btn.addEventListener('click', function(e){
-  
+    if(ul_list.children[0].className == "empty"){
+     ul_list.children[0].remove();
+    }
       let choose= e.currentTarget;
      
       let inp= choose.previousElementSibling.value;
@@ -15,11 +18,20 @@ btn.addEventListener('click', function(e){
                          <button onclick="closetheparen(this)">X</button>
                               `;
       list.classList.add("lists")
-      let ul_list= document.querySelector("#ul_list");
+      
       ul_list.appendChild(list);
 
 })
 
+
+
+
 function closetheparen(cuurentbtn){
      cuurentbtn.parentElement.remove();
+     if(ul_list.children.length <= 0){
+          let note= document.createElement("h2");
+          note.classList.add("empty")
+          note.textContent= "There Is Nothing To See Please Insert A Value"
+          ul_list.appendChild(note)
+     }
 }
