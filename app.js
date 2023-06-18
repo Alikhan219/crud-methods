4ff3452c52ddb763bb430ef5fdb67d37fd9e189b
Ali Khan <merabhai786786@gmail.com>
2023-06-18 15:22:22 +0500

    updating the code for same performance

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let ul_list= document.querySelector("#ul_list");
 let lists =document.querySelector(".lists")      
 let input= document.querySelector('.input');
 btn.addEventListener('click', function(e){
-    if(ul_list.children[0].className == "empty"){
-     ul_list.children[0].remove();
-    }
+     if(ul_list.children.length > 0){
+          note.remove();
+       }
       let choose= e.currentTarget;
      
       let inp= choose.previousElementSibling.value;
@@ -26,12 +26,13 @@ btn.addEventListener('click', function(e){
 
 
 
+let note= document.createElement("h3");
+          note.classList.add("empty")
+          note.textContent= "There Is Nothing To See Please Insert A Value"
+
 function closetheparen(cuurentbtn){
      cuurentbtn.parentElement.remove();
      if(ul_list.children.length <= 0){
-          let note= document.createElement("h2");
-          note.classList.add("empty")
-          note.textContent= "There Is Nothing To See Please Insert A Value"
           ul_list.appendChild(note)
      }
 }
